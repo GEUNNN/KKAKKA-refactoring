bff239f0e7b981a07fb93647e5453db26f12d16b
Geunhwa Lee <geunhwa.lee.01@gmail.com>
2022-02-24 23:51:16 +0900

    refactor: create main.tsx and connect to routes

--- refactoring/src/Routes.tsx
+++ refactoring/src/Routes.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Main from "./Pages/Main/Main.js";
+import { Main } from "./Pages/Main/Main.tsx";
 import Login from "./Pages/Login/Login.js";
 import Cart from "./Pages/Cart/Cart.js";
 import FlowerClassDetail from "./Pages/FlowerClassDetail/FlowerClassDetail.js";
