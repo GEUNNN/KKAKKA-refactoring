8e33052cfa12f7a20aebdeaf0e96e9b757e403ba
Geunhwa Lee <geunhwa.lee.01@gmail.com>
2022-02-25 23:35:54 +0900

    refactor: refactor state to useState

--- refactoring/src/config.js
+++ refactoring/src/config.js
@@ -3,7 +3,7 @@ export const config = {
 };
 export const mainSubscribeAPI = "http://13.125.156.179:8000/subscription";
 export const mockAPI = "http://localhost:3000/data/FlowerList.json";
-export const mainProductAPI = "http://13.125.156.179:8000/product/main";
+export const productMainAPI = "http://13.125.156.179:8000/product/main";
 export const loginAPI = "http://13.125.156.179:8000/account/signin";
 export const signupAPI = "http://13.125.156.179:8000/account/signup";
 export const mockCookieDataAPI = "http://localhost:3000/data/cookieData.json";
