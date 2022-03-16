bf6a5b735413a6c17a95801be320a956232adf72
Geunhwa Lee <geunhwa.lee.01@gmail.com>
2022-03-16 23:52:50 +0900

    refac: add return values and change variable name of image

--- refactoring/src/Pages/Main/Components/Cards/Cards.tsx
+++ refactoring/src/Pages/Main/Components/Cards/Cards.tsx
@@ -1,4 +1,3 @@
-import React, { Component } from "react";
 import "./Cards.scss";
 
 export interface CardsDataProps {
@@ -9,27 +8,13 @@ export interface CardsDataProps {
 }
 
 export function Cards({ name, description, image, price }: CardsDataProps) {
-  console.log(name);
-  return <div>Cards</div>;
+  return (
+    <div className="cards">
+      <img src={image} className="subscribeImg" alt="product" />
+      <p className="subscribeIntro">{description}</p>
+      <p className="subscribeName">{name}</p>
+      <p className="subscribePrice">{`${price}원`}</p>
+      <span className="freeShipping">무료배송</span>
+    </div>
+  );
 }
-
-// export class Cards extends Component {
-//   render() {
-//     const { description, name, price } = this.props;
-//     return (
-//       <div className="cards">
-//         <img
-//           src={this.props.subscribeImg}
-//           className="subscribeImg"
-//           alt="product"
-//         />
-//         <p className="subscribeIntro">{description}</p>
-//         <p className="subscribeName">{name}</p>
-//         <p className="subscribePrice">{`${price}원`}</p>
-//         <span className="freeShipping">무료배송</span>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Cards;
