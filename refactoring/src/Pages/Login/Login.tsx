51e465a13a68420970185c574d9b87e76e471f00
Geunhwa Lee <geunhwa.lee.01@gmail.com>
2022-03-24 22:56:45 +0900

    refactor: add login tsx file

---
+++ refactoring/src/Pages/Login/Login.tsx
@@ -0,0 +1,115 @@
+import { withRouter } from "react-router";
+import { Link } from "react-router-dom";
+import Nav from "../../Components/Nav/Nav";
+import { Footer } from "../../Components/Footer/Footer";
+import { loginAPI } from "../../config";
+import "./Login.scss";
+
+export function Login(): JSX.Element {
+  return <div>Login</div>;
+}
+
+// class Login extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       email: "",
+//       password: "",
+//       isValid: true,
+//     };
+//   }
+
+//   handleInput = e => {
+//     e.preventDefault();
+//     const { name, value } = e.target;
+//     this.setState({
+//       [name]: value,
+//     });
+//   };
+
+//   goToMain = e => {
+//     const regExpression =
+//       /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,12}$/i;
+
+//     if (
+//       regExpression.test(this.state.email) &&
+//       this.state.password.length >= 8
+//     ) {
+//       fetch(loginAPI, {
+//         method: "POST",
+//         body: JSON.stringify({
+//           id: this.state.email,
+//           password: this.state.password,
+//         }),
+//       })
+//         .then(response => response.json())
+//         .then(response => {
+//           console.log(response);
+//           if (response.message === "success_signin") {
+//             this.props.history.push("/main");
+//           } else if (response.message === "error_password_matching") {
+//             alert("비밀번호를 다시 입력해주세요");
+//           } else if (response.message === "error_mutiple_id" || "error_no_id") {
+//             alert("아이디(이메일)를 다시 입력해주세요");
+//           }
+//         })
+//         .then(response => {
+//           if (response.access_token) {
+//             sessionStorage.setItem("token", response.access_token);
+//           }
+//         });
+//     } else {
+//       this.setState({
+//         isValid: false,
+//       });
+//     }
+//     e.preventDefault();
+//   };
+
+//   render() {
+//     const { isValid } = this.state;
+//     return (
+//       <div className="login">
+//         <Nav />
+//         <div className="loginBox">
+//           <h1 className="loginText">로그인</h1>
+//           <form className="loginField">
+//             <input
+//               className="id"
+//               name="email"
+//               type="email"
+//               placeholder="아이디(이메일)"
+//               autoComplete="off"
+//               onChange={this.handleInput}
+//             />
+//             <input
+//               className="pwd"
+//               name="password"
+//               type="password"
+//               placeholder="비밀번호"
+//               onChange={this.handleInput}
+//             />
+//             {!isValid && (
+//               <div className="validation">
+//                 <p>Email/비밀번호를 정확히 입력해주세요.</p>
+//               </div>
+//             )}
+//             <div className="inputBtnBuffer"></div>
+//             <button className="loginBtn" onClick={this.goToMain}>
+//               로그인
+//             </button>
+//             <div className="signUpArea">
+//               <button className="signUpBtn">
+//                 <Link to="/signup" className="signUpLink">
+//                   회원가입
+//                 </Link>
+//               </button>
+//             </div>
+//           </form>
+//         </div>
+//         <Footer />
+//       </div>
+//     );
+//   }
+// }
+// export default withRouter(Login);
--- refactoring/src/Routes.tsx
+++ refactoring/src/Routes.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Main } from "./Pages/Main/Main";
-import Login from "./Pages/Login/Login.js";
+import { Login } from "./Pages/Login/Login";
 import Cart from "./Pages/Cart/Cart.js";
 import FlowerClassDetail from "./Pages/FlowerClassDetail/FlowerClassDetail.js";
 import FlowerClassList from "./Pages/FlowerClassList/FlowerClassList.js";
